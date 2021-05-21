61910303fa3cfe1e94989627e9541aed9e63ba48
Baghas Wahyu <82638738+BaghasWahyu@users.noreply.github.com>
2021-05-21 13:28:06 +0700

    Tugas No 2

--- tugas_2.py
+++ tugas_2.py
@@ -1,15 +1,11 @@
-# Created By Baghas at 5th Mei 2021
-
 from bs4 import BeautifulSoup as bs
 from requests.api import get
 import sys
 import os
 
-DOMAIN = "https://www.wavsource.com"
-TIPEFILE = ".wav"
-sys.stdout = open("link.txt", "w")
+# sys.stdout = open("link.txt", "w")
 
-folder = "C:\\Users\\Baghas\\Documents\\MRIS\\web_scraping\\tugas2"
+folder = "/content/Tugas-Besar-Bs4-1914311046/"
 for filename in os.listdir(folder):
     if filename.endswith(".html"):
         fname = os.path.join(folder, filename)
@@ -24,11 +20,4 @@ for filename in os.listdir(folder):
             if "https://www.wavsource.com/snds_2020-10-01" in new:
                 print(new)
 
-
-# file = open("page1.html", "r")
-# sys.stdout = open("test.txt", "w")
-# soup = bs(file, "html.parser")
-# for a in soup.find_all("a", href=True):
-
-
-sys.stdout.close()
+# sys.stdout.close()
